3483922479dc1cba3ad2ed8a9e4d2f405be22b07
nachopocho <nachoorfei12@gmail.com>
2024-04-17 12:02:51 -0300

    for

---
+++ 33.cpp
@@ -0,0 +1,37 @@
+/*
+    CICLO WHILE
+    IMPRIMIR LOS NUMEROS PARES
+*/
+
+///ESCRIBIR UN PROGRAMA QUE LE PIDA UN NUMERO ENTERO "N" AL USUARIO
+///Y QUE IMPRIMA TODOS LOS NUMEROS PARES HASTA ESE "N"
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+int main () {
+
+ int nro1;
+ int res;
+ cout<<"ingresar numero"<<endl;
+ cin>>nro1;
+
+for (int i=1; i<11; i++){
+
+  res=nro1*i;
+
+
+    cout<<(res)<<endl;
+    }
+
+
+
+
+
+
+return 0;
+}
